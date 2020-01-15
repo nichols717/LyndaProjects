85a781329b6d2826d917d6caabd1196d302f9193
nichols717 <nichols717@gmail.com>
2020-01-15 11:16:31 -0800

    Chapter 2 Exercises Complete

--- Exercise Files/Ch2/classes_start.py
+++ Exercise Files/Ch2/classes_start.py
@@ -1,9 +1,37 @@
 #
 # Example file for working with classes
 #
+# *** First argument to any method in a class is 'self'
+
+class myClass():
+  def method1(self):
+    print("myClass method1")
+
+  def method2(self, someString):
+    print("myClass method2 " + someString)
+
+# Inheritance is handle by adding the parent class as an argument to the child class
+class anotherClass(myClass):
+  def method1(self):
+    # Add 'self' to the args when referencing another class
+    myClass.method1(self)
+    print("anotherClass method1")
+
+  def method2(self, someString):
+    print("anotherClass method2 ")
+
 
 
 def main():
+  c = myClass()
+  c.method1()
+  c.method2("This is a string")
+
+  c2 = anotherClass()
+  c2.method1()
+  c2.method2("This is a string")
+  
+  
 
   
 if __name__ == "__main__":
--- Exercise Files/Ch2/loops_start.py
+++ Exercise Files/Ch2/loops_start.py
@@ -6,19 +6,33 @@ def main():
   x = 0
 
   # define a while loop
+  #while (x<5):
+  #  print (x)
+  #  x = x+1
 
 
   # define a for loop
-
+  #for x in range(5, 10):
+  #  print(x)
 
   # use a for loop over a collection
-    
+  #days=["Mon","Tues","Wed","Thu","Fri","Sat","Sun"]
+  #for d in days:
+  #  print(d)
+   
  
   # use the break and continue statements
-
+  # break statement exits the loop
+  # continue statement skips that iteration
+  #for x in range(5,10):
+  #  #if (x==7): break
+  #  if (x%2==0): continue
+  #  print(x)
 
   #using the enumerate() function to get index 
-
+  days=["Mon","Tues","Wed","Thu","Fri","Sat","Sun"]
+  for i,d in enumerate(days):
+    print(i, d)
 
 if __name__ == "__main__":
   main()
